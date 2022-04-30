eb37b84797c2ba9e1e43a44431cab4ca51bcd53d
Zach Underwood <zawood11@gmail.com>
2022-04-30 12:22:14 -0400

    completed assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   
   def index
+    byebug
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
